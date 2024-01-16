363916ccd8d9778ade21e6b8085773d54544be26
AbnerJosue <64668192+AbnerJosue@users.noreply.github.com>
2024-01-16 22:38:20 +0000

    Feat new changes

--- poo/proyecto/banca.py
+++ poo/proyecto/banca.py
@@ -4,7 +4,7 @@ class Persona:
         self.apellido = apellido
 
 class Cliente(Persona): 
-    def __init__(self, nombre, apellido, numero_cuenta, balance):
+    def __init__(self, nombre, apellido, numero_cuenta, balance = 0):
         super().__init__(nombre, apellido)
         self.numero_cuenta = numero_cuenta
         self.balance = balance
@@ -12,8 +12,7 @@ class Cliente(Persona):
     def __str__(self):
         return f"Datos del cliente:\n- Nombre completo: {self.nombre} {self.apellido}\n- Cuenta N°: {self.numero_cuenta}\n- Balance: {self.balance} "
 
-    def depositar(self):
-        deposito = int(input('Cuanto dinero desea agregar a la cuenta:'))
+    def depositar(self,deposito):
         if deposito < 0 : 
             print('No puedes ingresar una cantidad negativa')
         
@@ -21,8 +20,7 @@ class Cliente(Persona):
 
 
 
-    def retirar(self): 
-        retiro = int(input('Cuanto dinero desea retirar'))
+    def retirar(self, retiro): 
         if retiro > self.balance: 
             print("No tienes ese dinero disponible") 
             return
@@ -31,29 +29,29 @@ class Cliente(Persona):
             return
         self.balance -= retiro
 
+def crear_cliente(): 
+    nombre_cl = input("Ingrese su nombre: ")
+    apellido_cl = input("Ingrese su apellido: ")
+    numero_cuenta = input("Ingrese su numero de cuenta: ")
+    cliente = Cliente(nombre_cl, apellido_cl, numero_cuenta)
+    return cliente
 
+def inicio(): 
+    mi_cliente = crear_cliente()
+    print(mi_cliente)
+    opcion = 0
+    
+    while opcion != 'S': 
+        opcion = input('Elije: Depositar (D), Retirar (R), Salir (S): ')
+        
+        if opcion == "D": 
+            monto_dep = int(input('Monton a depositar: '))
+            mi_cliente.depositar(monto_dep)
+        elif opcion == "R":
+            monto_retirar = int(input("Monto a retirar")) 
+            mi_cliente.retirar(monto_retirar)
+        print(mi_cliente)
+    
+    print("Gracias por operar en Banco")
 
-elegir_opcion = ''
-nombre_cliente = 'Abner'
-apellido_cliente = 'Estrada'
-balance = 0
-numero_cuenta = '40239929993'
-
-ejecutar_accion = Cliente(nombre_cliente,apellido_cliente,numero_cuenta,balance)
-
-
-while elegir_opcion != '4':
-    print("""Seleccione las opciones: 
-        [1]: Depositar 
-        [2]: Retirar
-        [3]: Nombre de cuenta
-        [4]: Salir
-      """)
-    elegir_opcion = input('Selecciona: ')
-
-    if elegir_opcion == "1": 
-        ejecutar_accion.depositar()
-    elif elegir_opcion == "2": 
-        ejecutar_accion.retirar()
-    elif elegir_opcion == "3": 
-        print(str(ejecutar_accion))
+inicio()
